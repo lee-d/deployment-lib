018115d7f834ddd16136531f55c00c1efa5be9b3
Daniel Lee <info@lee-d.com>
2018-11-23 12:35:34 +0100

    asdf

--- vars/publishDependencyCheckDataToGrafana.groovy
+++ vars/publishDependencyCheckDataToGrafana.groovy
@@ -1,33 +1,27 @@
 @NonCPS
 def call(Map<String, Object> params) {
     def databaseName = params.databaseName
-    def url = params.url
-    def username = params.username
-    def password = params.password
-    def reportPath = params.reportPath ?: "${workspace}/target/dependency-check-report.xml"
     def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.DescriptorImpl)
 
     if (!influxdb.getTargets().any {it.description.equals(databaseName)}) {
         def target = new jenkinsci.plugins.influxdb.models.Target()
         target.description = databaseName
-        target.url = url
-        target.username = username
-        target.password = password
+        target.url = params.url
+        target.username = params.username
+        target.password = params.password
         target.database = databaseName
         influxdb.addTarget(target)
         influxdb.save()
     }
 
+    def reportPath = params.reportPath ?: "${workspace}/target/dependency-check-report.xml"
     def analysis = new XmlSlurper().parse(reportPath)
     def projectName = analysis.projectInfo.artifactID.text()
-
     def dependencyCheckMap = [:]
     def highestSeverityMap = [:]
     def highSeverityMap = [:]
-    def normalSeverityMap = [:]
     def highestSeverityCount = 0
     def highSeverityCount = 0
-    def normalSeverityCount = 0
 
     analysis.dependencies.dependency.each {
        highestSeverityCount = highestSeverityCount + it.vulnerabilities.vulnerability.count {
@@ -36,18 +30,13 @@ def call(Map<String, Object> params) {
        highSeverityCount = highSeverityCount + it.vulnerabilities.vulnerability.count {
           it.severity.text().toLowerCase().equals('high')
        }
-       normalSeverityCount = normalSeverityCount + it.vulnerabilities.vulnerability.count {
-        it.severity.text().toLowerCase().equals('medium')
-      }
 
        highSeverityMap[projectName] = highSeverityCount
        highestSeverityMap[projectName] = highestSeverityCount
-       normalSeverityMap[projectName] = normalSeverityCount
     }
 
     dependencyCheckMap['high-severity'] = highSeverityMap
     dependencyCheckMap['highest-severity'] = highestSeverityMap
-    dependencyCheckMap['normal-severity'] = normalSeverityMap
 
     step([$class: 'InfluxDbPublisher', target: databaseName, selectedTarget: databaseName, customPrefix: null, customDataMap: dependencyCheckMap])
 }
